9bd4fd4355cf18a72984edebf4ec202b63002679
Michael Beckwith <tw2113@gmail.com>
2012-02-14 20:40:39 -0600

    This is technically the first commit, but I am in a very sarcastic mood

---
+++ readme.txt
@@ -0,0 +1,1 @@
+oh hi! More coming soon, hopefully
---
+++ tweetquery-000001.js
@@ -0,0 +1,27 @@
+/*!
+ * tweetQuery
+ * Copyright: CC0 aka Public Domain
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ * .000001
+ * Requires: jQuery v1.3.2 or later
+ * Twitter API docs: https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
+ * Twitter Endpoint: https://api.twitter.com/1/statuses/user_timeline.json
+ */
+
+$.fn.tweetQuery = function(options) {
+	// set up default options
+	var defaults = {
+		username: 	"twitter", //twitter api version: screen_name
+		tweetcount:	5, //twitter api version: count
+		markup:		'div', // Available options: Div+p ul/ol+li, blockquote+cite
+		trimuser: 	1, //twitter api version: trim_user
+		retweets: 	1, //twitter api version: include_rts
+		entities: 	1, //twitter api version: include_entities
+		replies: 	1, //twitter api version: exclude replies
+	};
+
+	// Overwrite default options
+	// with user provided ones
+	// and merge them into "options".
+	var options = $.extend({}, defaults, options);
+}
